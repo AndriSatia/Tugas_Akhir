b69396012b965b43750efed668983a7c7bf5d1bf
Diva Dhila <divdhila10@gmail.com>
2024-06-13 17:18:42 +0700

    Front-end Changes

--- counter/myapp/static/script.js
+++ counter/myapp/static/script.js
@@ -1,14 +1,2 @@
-// Fungsi untuk pergi ke halaman beranda dengan URL yang ditentukan
-function goToHomePage(page) {
-  window.location.href = page;
-}
 
-// // Memeriksa apakah ada parameter mode di URL saat halaman dimuat
-// window.onload = function() {
-//   const urlParams = new URLSearchParams(window.location.search);
-//   const mode = urlParams.get('mode');
-//   if (mode) {
-//     document.getElementById('mode-title').textContent = 'Mode ' + mode;
-//   }
-// }
 
